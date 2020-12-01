371c739ebf2c840aaa9a9e21bb6482d866758c1d
liad <liadbenmoshe10@gmail.com>
2020-12-01 12:45:35 +0200

    finish level 2

--- Assets/Scripts/Attacks/PushHim.cs
+++ Assets/Scripts/Attacks/PushHim.cs
@@ -4,55 +4,36 @@ using UnityEngine;
 
 public class PushHim : MonoBehaviour
 {
-    public GameObject[] gos;
+   
     public float power = 500f;
     // Start is called before the first frame update
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            DoPush();
-        }
+       
     }
 
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.transform);
-        Debug.Log(other.gameObject);
+        Vector3 pos = transform.position;
+        Vector3 diff= other.gameObject.transform.position - pos;
+        Vector3 dir = new Vector3();
+        float curDistance = diff.sqrMagnitude;
+        dir = diff;
+        dir = dir.normalized;
+        if (other.gameObject.GetComponent<Rigidbody>())
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(dir * power);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(-dir * power);
+        }
     }
 
 
-    private void DoPush()
-    {
-
-        gos= GameObject.FindGameObjectsWithTag("Player");
-        GameObject closest = null;
-        float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
-        Vector3 dir = new Vector3();
-        foreach (GameObject go in gos)
-        {
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance)
-            {
-                closest = go;
-                distance = curDistance;
-                Debug.Log(closest);
-                dir = diff;
-            }
-        }
-        dir = dir.normalized;
-        if (distance < 0.9)
-        {
-            Debug.Log(distance);
-            closest.GetComponent<Rigidbody>().AddForce(dir * power);
-        }
+    
      
 
 
 
 
-    }
+    
 
 }
